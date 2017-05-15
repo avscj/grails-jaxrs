53a1718f77636209026532522f86fd062bb40f79
Alex <astoia1@sheffield.ac.uk>
2017-05-15 01:14:12 +0100

    TDD resource with multiple files under same key

--- jaxrs-example/grails-app/resources/com/budjb/TestMultipartResource.groovy
+++ jaxrs-example/grails-app/resources/com/budjb/TestMultipartResource.groovy
@@ -19,7 +19,7 @@ class TestMultipartResource {
     @Consumes(MediaType.MULTIPART_FORM_DATA )
     @Produces(MediaType.APPLICATION_JSON )
     @Path('/upload')
-    Response getTestMultipartRepresentation(
+    Response uploadFile(
         @FormDataParam('file') File file,
         @FormDataParam('file') FormDataBodyPart fileBodyPart) {
         // try with 
@@ -31,5 +31,30 @@ class TestMultipartResource {
             mimeType: fileBodyPart.getMediaType().toString()
         ] as JSON
         return Response.status(200).entity(ret).build()
+    }
+    
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA )
+    @Produces(MediaType.APPLICATION_JSON )
+    @Path('/uploadmultiple')
+    Response uploadMultipleFiles(
+        @FormDataParam('file') Collection<FormDataBodyPart> fileBodyParts) {
+        // try with 
+        // curl -v -X POST -H "Content-Type: multipart/form-data" -F "file=@src/integration-test/groovy/com/budjb/resources/pdf-sample.pdf" -F "file=@src/integration-test/groovy/com/budjb/resources/pdf-sample2.pdf" http://localhost:8060/api/testMultipart/uploadmultiple
+        Collection result = []
+        fileBodyParts.each { fileBodyPart ->
+            File file = fileBodyPart.getValueAs(File)
+            Map ret = [
+                name: fileBodyPart.getContentDisposition().getFileName(),
+                contentype: fileBodyPart.getContentDisposition().getType(),
+                creationDate: fileBodyPart.getContentDisposition().getCreationDate(),
+                modificationDate: fileBodyPart.getContentDisposition().getModificationDate(),
+                readDate: fileBodyPart.getContentDisposition().getReadDate(),
+                size: file.size(),
+                mimeType: fileBodyPart.getMediaType().toString()
+            ]
+            result.add(ret)
+        }
+        return Response.status(200).entity(result as JSON).build()
     }
 }
--- jaxrs-example/src/integration-test/groovy/com/budjb/TestMultipartResourceSpec.groovy
+++ jaxrs-example/src/integration-test/groovy/com/budjb/TestMultipartResourceSpec.groovy
@@ -8,15 +8,16 @@ import org.grails.plugins.jaxrs.test.JaxrsRequestProperties
 
 @Integration
 class TestMultipartResourceSpec extends JaxrsIntegrationSpec {
+
     def 'Ensure can send a multipart file to the resource'() {
         given: "A file to send"
         File file = new File('src/integration-test/groovy/com/budjb/resources/pdf-sample.pdf')
         
         when: "Sending a file to the web service"
         def response = makeRequest(new JaxrsRequestProperties(method: 'POST', 
-            uri: '/api/testMultipart/upload',
-            headers: ['Content-Type' : ['multipart/form-data']],
-            files: ['file': file]))
+                uri: '/api/testMultipart/upload',
+                headers: ['Content-Type' : ['multipart/form-data']],
+                files: ['file': [file]]))
         def result = new groovy.json.JsonSlurper().parseText(response.bodyAsString)
 
         then: "The response is correct"
@@ -25,7 +26,33 @@ class TestMultipartResourceSpec extends JaxrsIntegrationSpec {
         result.size > 0
         'application/pdf' == result.mimeType
     }
-        /**
+    
+    def 'Ensure can send multipart files to the resource under the same key'() {
+        given: "A file to send"
+        File pdfFile = new File('src/integration-test/groovy/com/budjb/resources/pdf-sample.pdf')
+        File imageFile = new File('src/integration-test/groovy/com/budjb/resources/image-sample.jpg')
+        
+        when: "Sending a file to the web service"
+        def response = makeRequest(new JaxrsRequestProperties(method: 'POST', 
+                uri: '/api/testMultipart/uploadmultiple',
+                headers: ['Content-Type' : ['multipart/form-data']],
+                files: ['file': [pdfFile, imageFile]]))
+        def result = new groovy.json.JsonSlurper().parseText(response.bodyAsString)
+
+        then: "The response is correct"
+        200 == response.status
+        
+        and: "The file datas are correct"
+        2 == result.size()
+        'pdf-sample.pdf' == result[0].name
+        7945 == result[0].size
+        'application/pdf' == result[0].mimeType
+        'image-sample.jpg' == result[1].name
+        89229 == result[1].size
+        'image/jpeg' == result[1].mimeType
+    }
+    
+    /**
      * Return the list of additional resources to build the JAX-RS servlet with.
      *
      * @return
